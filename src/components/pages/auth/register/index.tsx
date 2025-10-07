cf58d5a1e9b12779419551ef7e025c664dd8d9f1
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-10-08 01:10:40 +0530

    Update role options in registration forms and schemas to include 'backOffice'

--- src/components/pages/auth/register/index.tsx
+++ src/components/pages/auth/register/index.tsx
@@ -7,7 +7,7 @@ export function Register() {
 			{/* Right */}
 			<div className='flex flex-col gap-4 p-6 md:p-10'>
 				<div className='flex flex-1 items-center justify-center'>
-					<div className='w-full max-w-xs'>
+					<div className='w-full max-w-md'>
 						<RegisterForm />
 					</div>
 				</div>
